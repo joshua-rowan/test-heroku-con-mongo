c4f6462222414f2cadb696429574a256e15e543d
CardsClaw <joshr1979@gmail.com>
2023-08-22 18:02:15 -0500

    deploying

--- Solved/config/connection.js
+++ Solved/config/connection.js
@@ -5,6 +5,6 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
 
-connect(connectionString);
+mongoose.connect(connectionString);
 
 module.exports = connection;
